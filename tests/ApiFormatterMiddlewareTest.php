474f8381b28047d2b6738a41e34016354158173d
Alexandre Berthelot <alexandre.berthelot@donzat.fr>
2017-09-12 15:25:30 +0200

    Update tests

--- tests/ApiFormatterMiddlewareTest.php
+++ tests/ApiFormatterMiddlewareTest.php
@@ -3,7 +3,10 @@
 namespace Tests\EnderLab;
 
 use EnderLab\ApiFormatterMiddleware;
+use EnderLab\Dispatcher\Dispatcher;
+use GuzzleHttp\Psr7\ServerRequest;
 use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseInterface;
 
 class ApiFormatterMiddlewareTest extends TestCase
 {
@@ -11,5 +14,29 @@ class ApiFormatterMiddlewareTest extends TestCase
     {
         $apiFormatter = new ApiFormatterMiddleware();
         $this->assertInstanceOf(ApiFormatterMiddleware::class, $apiFormatter);
+    }
+
+    public function testParseFieldsParam()
+    {
+        $request = new ServerRequest(
+            'GET',
+            '/api/v1/users?fields=firstname,lastname,address(country,city)'
+        );
+        $delegate = new Dispatcher();
+        $apiFormatter = new ApiFormatterMiddleware();
+        $response = $apiFormatter->process($request, $delegate);
+        $this->assertInstanceOf(ResponseInterface::class, $response);
+    }
+
+    public function testParseSortParam()
+    {
+        $request = new ServerRequest(
+            'GET',
+            '/api/v1/users?sort=firstname,lastname&desc=age'
+        );
+        $delegate = new Dispatcher();
+        $apiFormatter = new ApiFormatterMiddleware();
+        $response = $apiFormatter->process($request, $delegate);
+        $this->assertInstanceOf(ResponseInterface::class, $response);
     }
 }
